d84bb0b4fa00f03c93dab6df2c6bc45526f0261e
Matheus Fernandes <matiugaorion@hotmail.com>
2024-03-03 17:38:42 -0300

    configuração inicial

---
+++ CRUDusuario/src/main/java/com/MatheusFernandes/CRUDusuario/CruDusuarioApplication.java
@@ -0,0 +1,13 @@
+package com.MatheusFernandes.CRUDusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDusuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDusuarioApplication.class, args);
+	}
+
+}
---
+++ CRUDusuario/src/test/java/com/MatheusFernandes/CRUDusuario/CruDusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MatheusFernandes.CRUDusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
